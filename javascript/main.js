0d8d6468bddce6c84d30bac669fa31760211de8f
rejRoky <rejaul.islam.roky@gmail.com>
2022-04-11 17:09:05 +0600

    JavaScript: form Added

--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,4 @@
-console.log('Hello world');
+/*console.log('Hello world');
 console.error('This is error');
 console.warn('this is warning');
 
@@ -310,3 +310,93 @@ console.log(person2Obj.getDOByear());
 console.log(person2Obj.getfullName());
 
 
+// DOM - Docoument Obeject Model
+
+// ELEMENT SELECTORS
+
+console.log(window);
+// window.alert(1);
+
+// Single Element Selectors
+console.log(document.getElementById('my-form'));
+console.log(document.querySelector('.container'));
+// Multiple Element Selectors
+console.log(document.querySelectorAll('.item'));
+console.log(document.getElementsByTagName('li'));
+console.log(document.getElementsByClassName('item'));
+
+const items = document.querySelectorAll('.item');
+items.forEach((item) => console.log(item));
+
+// MANIPULATING THE DOM
+const ul = document.querySelector('.items');
+// ul.remove();
+// ul.lastElementChild.remove();
+ul.firstElementChild.textContent = 'Hello';
+ul.children[1].innerText = 'Brad';
+ul.lastElementChild.innerHTML = '<h1>Hello</h1>';
+
+const btn = document.querySelector('.btn');
+// btn.style.background = 'red';
+
+// EVENTS
+
+// Mouse Event
+btn.addEventListener('click', e => {
+    e.preventDefault();
+    console.log(e.target.className);
+    document.getElementById('my-form').style.background = '#ccc';
+    document.querySelector('body').classList.add('bg-dark');
+    ul.lastElementChild.innerHTML = '<h1>Changed</h1>';
+});
+
+// Keyboard Event
+const nnameInput = document.querySelector('#name');
+nnameInput.addEventListener('input', e => {
+    document.querySelector('.container').append(nnameInput.value);
+});
+
+
+*/
+
+
+// USER FORM SCRIPT
+
+// Put DOM elements into variables
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
+
+// Listen for form submit
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
+    e.preventDefault();
+
+    if (nameInput.value === '' || emailInput.value === '') {
+        // alert('Please enter all fields');
+        msg.classList.add('error');
+        msg.innerHTML = 'Please enter all fields';
+
+        // Remove error after 3 seconds
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        // Create new list item with user
+        const li = document.createElement('li');
+
+        // Add text node with input values
+        li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
+
+        // Add HTML
+        // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
+
+        // Append to ul
+        userList.appendChild(li);
+
+        // Clear fields
+        nameInput.value = '';
+        emailInput.value = '';
+    }
+}
